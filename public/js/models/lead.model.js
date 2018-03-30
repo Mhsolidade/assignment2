21fc623ef7a679640d6b92e58ea3d2705a034e4d
matheus solidade <mhsolidade@gmail.com>
2018-03-30 16:30:21 -0300

    model js leads

--- public/js/models/lead.model.js
+++ public/js/models/lead.model.js
@@ -1,7 +1,11 @@
-
 class LeadModel {
-    
-    constructor() {
-        
-    }
+  constructor(nome, email, contato) {
+    this.nome = nome;
+    this.email = email;
+
+    this.contato = contato;
+    this.ip = "nome";
+    //criar get ip
+    this.data = new Date();
+  }
 }
