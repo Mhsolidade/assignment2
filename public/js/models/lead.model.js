a4404cdd0de892b9c608e845547a622598097afb
VagnerSilva <vagner23silva@gmail.com>
2018-03-30 17:10:10 -0300

    add ip

--- public/js/models/lead.model.js
+++ public/js/models/lead.model.js
@@ -2,10 +2,8 @@ class LeadModel {
   constructor(nome, email, contato) {
     this.nome = nome;
     this.email = email;
-
     this.contato = contato;
-    this.ip = "nome";
-    //criar get ip
+    this.ip = AddressService.ip;
     this.data = new Date();
     
   }
