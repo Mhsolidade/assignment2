8eab5b600799438b5386fe842e618661ef7b25e7
VagnerSilva <vagner23silva@gmail.com>
2018-03-30 04:02:15 -0300

    voltando as origens

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,10 @@
 
 import { ContatoController } from './controllers/ContatoController'
 import '../css/style.css'
+import designerIcon from  '../images/'
 
+
+var homeImg = document.getElementById('designer');
+homeImg.src = designerIcon;
 
 let contatoController = new ContatoController();
