eb5666d7fefff55835d0d667c35d1b41f62b87f9
VagnerSilva <vagner23silva@gmail.com>
2018-03-31 00:06:15 -0300

    finalizando persistencia

--- public/js/controllers/lead.controller.js
+++ public/js/controllers/lead.controller.js
@@ -12,9 +12,12 @@ class LeadController {
 
         let nome = $('#nome').val();
         let email = $('#email').val();
+        let social = $('#social').val();
         let tipo = $('input[name=lead-alvo]:checked', '#myForm').val();
-        let lead = new LeadModel(nome, email, tipo);
+        let lead = new LeadModel(nome, email, social, tipo);
         this.leadService.create(lead)
+
+        return false;
 
     }
 }
--- public/js/models/lead.model.js
+++ public/js/models/lead.model.js
@@ -1,12 +1,32 @@
 class LeadModel {
-  constructor(nome, email, tipo) {
-    console.log(nome);
+  constructor(nome, email, social, tipo) {
     this.email = email;
     this.nome = nome;
+    this.social = social;
     this.ip = HelperService.ip;
     this.tipo = tipo;
     this.data = HelperService.data;
+    this.respostas = this._requestResponse;
   }
-}
 
+  get _requestResponse() {
+
+    const rq = {};
+    let resp;
+    // $('input[name=lead-alvo]:checked', '#myForm').val()
+    const op = $('#opcao label.active input').val();   //$('input[name="problema"]:checked', "#myForm").val();
+    const comoLidar = $("#comoLidar").val();
+
+    op == 'Sim' ?  resp = `${op}, ${comoLidar}` : resp = `${op}`;
+    rq['Você já teve problema com análise de competências técnicas em um processo seletivo?'] = resp;
+    rq['qual seria a solução ideal'] = $("#solucao").val();
+
+    console.log(rq);
+    return rq;
+  }
+
+ 
+
+
+}
 
