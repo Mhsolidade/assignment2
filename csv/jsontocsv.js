9dbc45f710035bf1f63d9d12b3e0e02eca362557
VagnerSilva <vagner23silva@gmail.com>
2018-04-01 20:42:08 -0300

    fix  dir csv

--- csv/jsontocsv.js
+++ csv/jsontocsv.js
@@ -9,20 +9,22 @@ const fields = ['email', 'nome', 'ip', 'tipo', 'data_hora'];
 
 
 async function generetorCsv() {
+    console.log('dir')
+    console.log(path.dirname(__dirname))
   request.get("https://gamaassigment2.firebaseio.com/lista/leads.json", function (error, response, body) {  
             const dados = JSON.parse(body);
             const json = Object.keys(dados).map(id => dados[id]);
-            
+    
             const json2csvParser = new Json2csvParser({ fields });
             const csv = json2csvParser.parse(json);
             try {
-                fs.unlinkSync('../leads.csv');
+                fs.unlinkSync(path.dirname(__dirname) + "/leads.csv");
                 
             } catch (error) {
                 
             }
-            fs.writeFileSync('../leads.csv',csv,"utf8" );
-            console.log(`seu arquivo foi salvo no diretorio ${path.join(__dirname,"../leads.csv")}` );
+            fs.writeFileSync(path.dirname(__dirname) + "/leads.csv", csv, "utf8" );
+            console.log(`Seu arquivo foi salvo no diretorio ${path.dirname(__dirname) +"/leads.csv"}` );
     });
 
 }
