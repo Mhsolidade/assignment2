b0a02b627923e77b94483671317479b9d48a8a18
matheus solidade <mhsolidade@gmail.com>
2018-04-01 22:08:16 -0300

    merge

--- public/js/controllers/lead.controller.js
+++ public/js/controllers/lead.controller.js
@@ -14,7 +14,7 @@ class LeadController {
         let email = $('#email').val();
         // let social = $('#social').val();
         let tipo = $('#lead-alvo').val(); //$('input[name=lead-alvo]:checked', '#myForm').val();
-        let lead = new LeadModel(nome, email, social, tipo);
+        let lead = new LeadModel(nome, email, tipo);
         this.leadService.create(lead);
 
         $('#myForm')[0].reset();
