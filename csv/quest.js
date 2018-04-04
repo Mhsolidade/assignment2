860ab0fbedf86f5621b13c40506ee7dc925b5f66
VagnerSilva <vagner23silva@gmail.com>
2018-04-03 22:08:32 -0300

    add  csv quest

--- csv/quest.js
+++ csv/quest.js
@@ -4,16 +4,17 @@ const Json2csvParser = require('json2csv').Parser;
 const fs = require('fs');
 const path = require('path');
 
+var jsonexport = require('jsonexport');
+
+
 const file = path.dirname(__dirname) + "/quest.csv";
 const URL = "https://gamaassigment2.firebaseio.com/lista/leads.json";
 
 const fields = [
     'email',
-    'nome',
-    'ip',
-    'tipo',
-    'data_hora',
-    'respostas["Você já teve problema(s) com análises de competências técnicas?"]'
+    'respostas["Você já teve problema(s) com análises de competências técnicas?"]',
+    'respostas["Como você lidou ou lida com isso?"]',
+    'respostas["Qual seria a solução ideal?"]'
 ];
 
 function get(url) {
@@ -24,22 +25,47 @@ function get(url) {
     });
 }
 
+
 get(URL).then(dados => {
-    get(URL).then(dados => {
-        ;
-        const json = Object.keys(dados).map(id => dados[id]);
 
-        const json2csvParser = new Json2csvParser({ fields });
-        const csv = json2csvParser.parse(json);
+    const json = Object.keys(dados).map(id => dados[id]);
+    const email = 'vagner.silva@designermatch.com.br';
+    // const respostas = json.map(resp => resp.respostas); 
 
-        // remove file
-        try {
-            fs.unlinkSync(file);
-        } catch (error) { }
+ 
+    jsonexport(json,function(err, csv){
+        if(err) return console.log(err);
+       /// console.log(csv);
+           // remove file
+    try {
+        fs.unlinkSync(file);
+    } catch (error) { }
 
-        fs.writeFileSync(file, csv, "utf8");
-        console.log(`Seu arquivo foi salvo no diretorio ${file}`);
-    }).catch(error => {
-        console.log(`Um erro ocorreu: ${error}`);
+    //   console.log(csv) // ok;
+
+    fs.writeFileSync(file, csv, { encoding: 'ascii' });
+    console.log(`Seu arquivo foi salvo no diretorio ${file}`);
     });
+
+
+
+
+
+
+    // const json2csvParser = new Json2csvParser({ fields });
+    // const csv = json2csvParser.parse(json);
+
+    // // remove file
+    // try {
+    //     fs.unlinkSync(file);
+    // } catch (error) { }
+
+    // //   console.log(csv) // ok;
+
+    // fs.writeFileSync(file, csv.toString(), { encoding: 'ascii' });
+    // console.log(`Seu arquivo foi salvo no diretorio ${file}`);
+
+
+}).catch(error => {
+    console.log(`Um erro ocorreu: ${error}`);
 });
